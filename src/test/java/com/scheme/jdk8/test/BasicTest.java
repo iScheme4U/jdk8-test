784fec2385ac934ab2f009d9a28ddacb25a8ebeb
Scott Lau <exceedego@126.com>
2022-11-21 18:12:55 +0800

    test DiscardPolicy

--- src/test/java/com/scheme/jdk8/test/BasicTest.java
+++ src/test/java/com/scheme/jdk8/test/BasicTest.java
@@ -10,6 +10,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.temporal.TemporalAccessor;
 import java.util.*;
+import java.util.concurrent.*;
 
 public class BasicTest {
     private static final Logger log = LogManager.getLogger(BasicTest.class.getName());
@@ -252,5 +253,35 @@ public class BasicTest {
         }
         strs[0] = "2";
         System.out.println(strings);
+    }
+
+    @Test
+    public void testThreadPool() throws ExecutionException, InterruptedException {
+        // 一个核心线程，队列最大为1，最大线程数也是1.拒绝策略是DiscardPolicy
+        ThreadPoolExecutor executorService = new ThreadPoolExecutor(1, 1, 1L, TimeUnit.MINUTES,
+                new ArrayBlockingQueue<>(1), new ThreadPoolExecutor.DiscardPolicy());
+
+        Future f1 = executorService.submit(()-> {
+            System.out.println("提交任务1");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Future f2 = executorService.submit(()->{
+            System.out.println("提交任务2");
+        });
+
+        Future f3 = executorService.submit(()->{
+            System.out.println("提交任务3");
+        });
+
+        System.out.println("任务1完成 " + f1.get());// 等待任务1执行完毕
+        System.out.println("任务2完成 " + f2.get());// 等待任务2执行完毕
+        System.out.println("任务3完成 " + f3.get());// 等待任务3执行完毕
+
+        executorService.shutdown();// 关闭线程池，阻塞直到所有任务执行完毕
     }
 }
