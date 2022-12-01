2dbaedc66312018c7a1bb699a1a293dfc6be0563
Scott Lau <exceedego@126.com>
2022-12-01 19:05:51 +0800

    测试增量打包时的常量引用问题

---
+++ src/test/java/com/scheme/jdk8/test/ClassA.java
@@ -0,0 +1,7 @@
+package com.scheme.jdk8.test;
+
+public class ClassA {
+    public static final String A = "A";
+    public static final String A1 = String.valueOf("A");
+
+}
---
+++ src/test/java/com/scheme/jdk8/test/ClassB.java
@@ -0,0 +1,8 @@
+package com.scheme.jdk8.test;
+
+public class ClassB {
+    public static void main(String[] args) {
+        System.out.println(ClassA.A);
+        System.out.println(ClassA.A1);
+    }
+}
